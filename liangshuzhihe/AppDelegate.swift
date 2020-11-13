8e577fa139c0fe88330b27ffde1b98ce15ad4a12
zhangliqun <zhangliuqn@xiangshang360.com>
2020-11-13 16:20:59 +0800

    first

---
+++ liangshuzhihe/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  liangshuzhihe
+//
+//  Created by UED on 2020/10/30.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
