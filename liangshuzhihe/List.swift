3e2b80251eb43c9bda0814a8161cb67849d7b947
zhangliqun <zhangliuqn@xiangshang360.com>
2021-04-16 10:11:10 +0800

    移除数组

--- liangshuzhihe/List.swift
+++ liangshuzhihe/List.swift
@@ -152,4 +152,19 @@ class List : NSObject {
         }
         return i + 1;
     }
+    
+    /// 移除数组
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        var i = 0;
+        var n = nums.count;
+        while i < n {
+            if nums[i] == val {
+                nums[i] = nums[n - 1];
+                n -= 1;
+            } else {
+                i += 1;
+            }
+        }
+        return n;
+    }
 }
