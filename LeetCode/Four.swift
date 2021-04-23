ccd6b51f08d567d70abcc3b6862b46bcccf42e12
zhangliqun <zhangliuqn@xiangshang360.com>
2021-04-23 15:42:26 +0800

    搜索插入位置

--- LeetCode/Four.swift
+++ LeetCode/Four.swift
@@ -117,4 +117,20 @@ class Four: NSObject {
         return [-1, -1]
     }
     
+    // 35. 搜索插入位置
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        let n = nums.count
+        var left = 0, right = n - 1, ans = n
+        while left <= right {
+            let mid = (left + right) / 2
+            if target <= nums[mid] {
+                ans = mid
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        return ans
+    }
+    
 }
