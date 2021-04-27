fbb1c47f553e38468e486ec6a199c5b3e270d626
zhangliqun <zhangliuqn@xiangshang360.com>
2021-04-27 14:43:24 +0800

    接雨水

--- LeetCode/Four.swift
+++ LeetCode/Four.swift
@@ -30,4 +30,23 @@ class Four: NSObject {
         }
         return n + 1
     }
+    
+    // 42. 接雨水
+    func trap(_ height: [Int]) -> Int {
+        var ans = 0
+        var left = 0, right = height.count - 1
+        var leftMax = 0, rightMax = 0
+        while left < right {
+            leftMax = max(leftMax, height[left])
+            rightMax = max(rightMax, height[right])
+            if height[left] < height[right] {
+                ans += leftMax - height[left];
+                left += 1
+            } else {
+                ans += rightMax - height[right]
+                right -= 1
+            }
+        }
+        return ans
+    }
 }
