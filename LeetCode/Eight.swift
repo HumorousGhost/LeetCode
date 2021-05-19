25c9b638ab46796725e2173a0b55c225f6deb07f
zhangliqun <zhangliuqn@xiangshang360.com>
2021-05-19 15:02:41 +0800

    81. 搜索旋转排序数组 II

--- LeetCode/Eight.swift
+++ LeetCode/Eight.swift
@@ -22,4 +22,42 @@ class Eight: NSObject {
         }
         return nums.count
     }
+    
+    // 81. 搜索旋转排序数组 II
+    func search(_ nums: [Int], _ target: Int) -> Bool {
+        
+        let n = nums.count
+        if n == 0 {
+            return false
+        }
+        
+        if n == 1 {
+            return nums[0] == target
+        }
+        
+        var l = 0, r = n - 1
+        while l <= r {
+            let mid = (l + r) / 2
+            if nums[mid] == target {
+                return true
+            }
+            if nums[l] == nums[mid] && nums[mid] == nums[r] {
+                l += 1
+                r -= 1
+            } else if nums[l] <= nums[mid] {
+                if nums[l] <= target && target < nums[mid] {
+                    r = mid - 1
+                } else {
+                    l = mid + 1
+                }
+            } else {
+                if nums[mid] < target && target <= nums[r] {
+                    l = mid + 1
+                } else {
+                    r = mid - 1
+                }
+            }
+        }
+        return false
+    }
 }
--- LeetCode/ViewController.swift
+++ LeetCode/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: NSViewController {
 
         // Do any additional setup after loading the view.
         var arr = [1,1,1,2,2,3]
-        let result = Eight().removeDuplicates(&arr)
+        let result = Eight().search([2,5,6,0,0,1,2], 1)
         print(result)
-        print(arr)
+//        print(arr)
     }
 
     override var representedObject: Any? {
