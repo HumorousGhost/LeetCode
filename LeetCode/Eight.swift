89cdec65fc0a4aa5bbc027dc9767f036f8bd2821
zhangliqun <zhangliuqn@xiangshang360.com>
2021-05-19 15:34:52 +0800

    82. 删除排序链表中的重复元素 II

--- LeetCode/Eight.swift
+++ LeetCode/Eight.swift
@@ -60,4 +60,27 @@ class Eight: NSObject {
         }
         return false
     }
+    
+    // 82. 删除排序链表中的重复元素II
+    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+        if head == nil {
+            return head
+        }
+        let dummy = ListNode.init(0, head)
+        
+        var cur: ListNode? = dummy
+        
+        while cur?.next != nil && cur?.next?.next != nil {
+            if cur?.next?.val == cur?.next?.next?.val {
+                let x = cur?.next?.val
+                while cur?.next != nil && cur?.next?.val == x {
+                    cur?.next = cur?.next?.next
+                }
+            } else {
+                cur = cur?.next
+            }
+        }
+        
+        return dummy.next
+    }
 }
--- LeetCode/ViewController.swift
+++ LeetCode/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: NSViewController {
 
         // Do any additional setup after loading the view.
         var arr = [1,1,1,2,2,3]
-        let result = Eight().search([2,5,6,0,0,1,2], 1)
+        let list = ListNode.init(1, ListNode.init(2, ListNode.init(3, ListNode.init(3, ListNode.init(4, ListNode.init(4, ListNode.init(5, nil)))))))
+        let result = Eight().deleteDuplicates(list)
         print(result)
 //        print(arr)
     }
