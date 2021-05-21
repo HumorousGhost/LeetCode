9f4c6f241bff17a76345bb4689d5c8b1d1b5b44b
zhangliqun <zhangliuqn@xiangshang360.com>
2021-05-21 15:58:32 +0800

    88. 合并两个有序数组

--- LeetCode/Eight.swift
+++ LeetCode/Eight.swift
@@ -285,4 +285,12 @@ class Eight: NSObject {
         
         return dfs(0, 0, length)
     }
+    
+    //88. 合并两个有序数组
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        for i in 0..<n {
+            nums1[m + i] = nums2[i]
+        }
+        nums1.sort()
+    }
 }
