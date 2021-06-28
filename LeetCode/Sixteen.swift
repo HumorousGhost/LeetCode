53d3d0162dca4043647e6fd96a04870754e40431
zhangliqun <zhangliuqn@xiangshang360.com>
2021-06-28 15:10:10 +0800

    167. 两数之和 II - 输入有序数组

--- LeetCode/Sixteen.swift
+++ LeetCode/Sixteen.swift
@@ -163,4 +163,20 @@ class Sixteen {
         }
         return fraction.joined()
     }
+    
+    // 167. 两数之和 II - 输入有序数组
+    func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        var low = 0, high = numbers.count - 1
+        while low < high {
+            let sum = numbers[low] + numbers[high]
+            if sum == target {
+                return [low + 1, high + 1]
+            } else if sum < target {
+                low += 1
+            } else {
+                high -= 1
+            }
+        }
+        return [-1, -1]
+    }
 }
