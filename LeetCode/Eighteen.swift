518a5493ba691230e5f311783d6d78f5573e11cf
zhangliqun <zhangliuqn@xiangshang360.com>
2021-07-02 15:28:51 +0800

    182. 查找重复的电子邮箱

--- LeetCode/Eighteen.swift
+++ LeetCode/Eighteen.swift
@@ -33,4 +33,14 @@ class Eighteen {
         AND a.Salary > b.Salary
     ;
      */
+    
+    // 182. 查找重复的电子邮箱
+    /**
+    SELECT
+        Email
+    FROM Person
+        GROUP BY Email
+        HAVING COUNT(Email) > 1
+    ;
+     */
 }
