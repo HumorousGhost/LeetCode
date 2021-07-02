8b68616366eee49b9a63e8c5fc2eb36bdb01739c
zhangliqun <zhangliuqn@xiangshang360.com>
2021-07-02 14:50:43 +0800

    180. 连续出现的数字

---
+++ LeetCode/Eighteen.swift
@@ -0,0 +1,26 @@
+//
+//  Eighteen.swift
+//  LeetCode
+//
+//  Created by UED on 2021/7/1.
+//
+
+import Foundation
+
+class Eighteen {
+    // 连续出现的数字
+    /**
+     SELECT DISTINCT
+        l1.Num AS ConsecutiveNums
+     FROM
+        Logs l1,
+        Logs l2,
+        Logs l3
+     WHERE
+        l1.Id = l2.Id - 1
+        AND l2.Id = l3.Id - 1
+        AND l1.Num = l2.Num
+        AND l2.Num = l3.Num
+     ;
+     */
+}
