150ff1c6c76cf7cab7b735d3b4b2336d06ce6829
zhangliqun <zhangliuqn@xiangshang360.com>
2021-07-08 15:33:48 +0800

    194. 转置文件

--- LeetCode/Nineteen.swift
+++ LeetCode/Nineteen.swift
@@ -39,4 +39,15 @@ class Nineteen {
     /**
      awk '/^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$/' file.txt
      */
+    
+    // 194. 转置文件
+    /**
+     # 获取第一行，然后用wc来获取列数
+     COLS=`head -1 file.txt | wc -w`
+     # 使用awk依次去输出文件的每一列的参数，然后用xargs做转置
+     for (( i = 1; i <= $COLS; i++ )); do
+         # 这里col就是在代码里要替换的参数，而它等于$i
+         awk -v col=$i '{print $col}' file.txt | xargs
+     done
+     */
 }
