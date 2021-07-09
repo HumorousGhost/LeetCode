602b3c69ae19e83df9f2053e07a6a198406d2563
zhangliqun <zhangliuqn@xiangshang360.com>
2021-07-09 14:42:27 +0800

    196. 删除重复的电子邮箱

--- LeetCode/Nineteen.swift
+++ LeetCode/Nineteen.swift
@@ -55,4 +55,15 @@ class Nineteen {
     /**
      sed -n "10p" file.txt
      */
+    
+    // 196. 删除重复的电子邮箱
+    /**
+    DELETE p1
+    FROM Person p1,
+        Person p2
+    WHERE
+        p1.Email = p2.Email
+    AND p1.Id > p2.Id
+    ;
+     */
 }
