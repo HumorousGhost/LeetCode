3b849b66a7a71c53cdb36eae9336b91355f90024
zhangliqun <zhangliuqn@xiangshang360.com>
2021-07-14 15:14:06 +0800

    206. 反转链表

--- LeetCode/Twenty.swift
+++ LeetCode/Twenty.swift
@@ -140,4 +140,17 @@ class Twenty {
         }
         return true
     }
+    
+    // 206. 反转链表
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        var prev: ListNode? = nil
+        var curr = head
+        while curr != nil {
+            let next = curr?.next
+            curr?.next = prev
+            prev = curr
+            curr = next
+        }
+        return prev
+    }
 }
