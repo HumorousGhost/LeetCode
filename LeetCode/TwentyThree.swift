dfab1567daa7f92319f8cdb5bf8790fdcf136116
zhangliqun <zhangliuqn@xiangshang360.com>
2021-07-26 15:37:06 +0800

    230. 二叉搜索树中第K小的元素

---
+++ LeetCode/TwentyThree.swift
@@ -0,0 +1,28 @@
+//
+//  TwentyThree.swift
+//  LeetCode
+//
+//  Created by UED on 2021/7/26.
+//
+
+import Foundation
+
+class TwentyThree {
+    // 230. 二叉搜索树中第K小的元素
+    func kthSmallest(_ root: TreeNode?, _ k: Int) -> Int {
+        var nums = [Int]()
+        
+        func tree(_ root: TreeNode?) {
+            guard root != nil else {
+                return
+            }
+            tree(root?.left)
+            nums.append(root!.val)
+            tree(root?.right)
+        }
+        
+        tree(root)
+        
+        return nums[k - 1]
+    }
+}
