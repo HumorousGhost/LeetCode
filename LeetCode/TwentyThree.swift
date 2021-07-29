4f03575ced1a9ed23f805187017a3b343e0f80e9
zhangliqun <zhangliuqn@xiangshang360.com>
2021-07-29 15:13:23 +0800

    238. 除自身以外数组的乘积

--- LeetCode/TwentyThree.swift
+++ LeetCode/TwentyThree.swift
@@ -142,4 +142,37 @@ class TwentyThree {
         node?.val = node?.next?.val ?? -1
         node?.next = node?.next?.next
     }
+    
+    // 238. 除自身以外数组的乘积
+    func productExceptSelf(_ nums: [Int]) -> [Int] {
+        guard nums.count != 0 || nums.count != 1 else {
+            return []
+        }
+        
+        let count = nums.count
+        
+        // L 和 R 分别表示左右两侧的乘积列表
+        var L = [Int](repeating: 1, count: count)
+        var R = [Int](repeating: 1, count: count)
+        
+        var answer = [Int](repeating: 1, count: count)
+        
+        // L[i] 为索引 i 左侧所有元素的乘积
+        // 对于索引 '0' 的元素，因为左侧没有元素，所以 L[0] = 1
+        for i in 1..<count {
+            L[i] = nums[i - 1] * L[i - 1]
+        }
+        
+        // R[i] 为索引 i 右侧所有元素的乘积
+        // 对于索引为 'count - 1' 的元素，因为右侧没有元素，所以 R[count - 1] = 1
+        for i in stride(from: count - 2, through: 0, by: -1) {
+            R[i] = nums[i + 1] * R[i + 1]
+        }
+        
+        // 对于索引 i，除 nums[i] 之外其余各元素的乘积就是左侧所有元素的乘积乘以右侧所有元素的乘积
+        for i in 0..<count {
+            answer[i] = L[i] * R[i]
+        }
+        return answer
+    }
 }
