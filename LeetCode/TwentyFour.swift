d7aa67d2c271149734b7df6b5e4c5ee595945eb1
zhangliqun <zhangliuqn@xiangshang360.com>
2021-07-30 15:27:06 +0800

    242. 有效的字母异位词

--- LeetCode/TwentyFour.swift
+++ LeetCode/TwentyFour.swift
@@ -130,4 +130,24 @@ class TwentyFour {
         return optAry.sorted()
     }
     
+    // 242. 有效的字母异位词
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        guard s.count == t.count else {
+            return false
+        }
+        
+        var map = [Character: Int]()
+        for char in s {
+            map[char, default: 0] += 1
+        }
+        
+        for char in t {
+            map[char, default: 0] -= 1
+            if map[char]! < 0 {
+                return false
+            }
+        }
+        
+        return true
+    }
 }
