32db4cbac765ed0f6d68bcff92a1d8b72cdb0c44
zhangliqun <zhangliuqn@xiangshang360.com>
2021-08-02 14:57:43 +0800

    257. 二叉树的所有路径

---
+++ LeetCode/TwentyFive.swift
@@ -0,0 +1,39 @@
+//
+//  TwentyFive.swift
+//  LeetCode
+//
+//  Created by UED on 2021/7/30.
+//
+
+import Foundation
+
+class TwentyFive {
+    // 257. 二叉树的所有路径
+    func binaryTreePaths(_ root: TreeNode?) -> [String] {
+        
+        var paths = [String]()
+        
+        func constructPaths(_ root: TreeNode?, _ path: String) {
+            guard root != nil else {
+                return
+            }
+            var path = path
+            path.append("\(root!.val)")
+            // 当前节点是叶子节点
+            if root?.left == nil && root?.right == nil {
+                // 把路径加入到答案中
+                paths.append(path)
+            }
+            // 当前节点不是叶子节点，继续递归遍历
+            else {
+                path.append("->")
+                constructPaths(root?.left, path)
+                constructPaths(root?.right, path)
+            }
+        }
+        
+        constructPaths(root, "")
+        
+        return paths
+    }
+}
