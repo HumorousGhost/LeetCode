9a14b17d5a8ea2da65891219f6ebf9251e8920d0
zhangliqun <zhangliuqn@xiangshang360.com>
2021-10-08 10:14:26 +0800

    342. 4的幂

--- LeetCode/ThirtyFour.swift
+++ LeetCode/ThirtyFour.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+class ThirtyFour {
+    
+    // 342. 4的幂
+    func isPowerOffFour(_ n: Int) -> Bool {
+        
+        return n > 0 && (n & (n - 1)) == 0 && n % 3 == 1
+    }
+}
