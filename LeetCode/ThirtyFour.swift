6c2c8a567bcd14e1e8dcefcfd16675d1e87ee502
zhangliqun <zhangliuqn@xiangshang360.com>
2021-10-09 10:02:29 +0800

    343. 整数拆分

--- LeetCode/ThirtyFour.swift
+++ LeetCode/ThirtyFour.swift
@@ -14,4 +14,17 @@ class ThirtyFour {
         
         return n > 0 && (n & (n - 1)) == 0 && n % 3 == 1
     }
+    
+    // 343. 整数拆分
+    func integerBreak(_ n: Int) -> Int {
+        var dp = [Int](repeating: 0, count: n + 1)
+        for i in 2...n {
+            var curMax = 0
+            for j in 1..<i {
+                curMax = max(curMax, max(j * (i - j), j * dp[i - j]));
+            }
+            dp[i] = curMax
+        }
+        return dp[n]
+    }
 }
