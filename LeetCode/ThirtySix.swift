5569fc9e712c13305691de969fa3dd0c0204ce70
zhangliqun <zhangliuqn@xiangshang360.com>
2021-10-25 14:44:27 +0800

    365. 水壶问题

--- LeetCode/ThirtySix.swift
+++ LeetCode/ThirtySix.swift
@@ -37,4 +37,27 @@ class ThirtySix {
         }
         return maxNum
     }
+    
+    // 365. 水壶问题
+    func canMeasureWater(_ jug1Capacity: Int, _ jug2Capacity: Int, _ targetCapacity: Int) -> Bool {
+        
+        func gcd(_ x: Int, _ y: Int) -> Int {
+            var x = x, y = y
+            var remainder = x % y
+            while remainder != 0 {
+                x = y
+                y = remainder
+                remainder = x % y
+            }
+            return y
+        }
+        
+        if jug1Capacity + jug2Capacity < targetCapacity {
+            return false
+        }
+        if jug1Capacity == 0 || jug2Capacity == 0 {
+            return targetCapacity == 0 || jug1Capacity + jug2Capacity == targetCapacity
+        }
+        return targetCapacity % gcd(jug1Capacity, jug2Capacity) == 0
+    }
 }
