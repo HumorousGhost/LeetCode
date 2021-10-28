da866ef79b42581dcd998cfd44c369d52ead4a2a
zhangliqun <zhangliuqn@xiangshang360.com>
2021-10-28 15:05:42 +0800

    372. 超级次方

--- LeetCode/ThirtySeven.swift
+++ LeetCode/ThirtySeven.swift
@@ -18,4 +18,31 @@ class ThirtySeven {
         }
         return a
     }
+    
+    // 372. 超级次方
+    func superPow(_ a: Int, _ b: [Int]) -> Int {
+        
+        let mod = 1337
+        
+        func modPow(_ base: Int, _ exponent: Int) -> Int {
+            var ans = 1, base = base, exponent = exponent
+            
+            while exponent > 0 {
+                if exponent & 1 == 1 {
+                    ans = (base * ans) % mod
+                }
+                base = (base * base) % mod
+                exponent >>= 1
+            }
+            return ans
+        }
+        
+        var base = a % mod, ans = 1, x = base
+        
+        b.reversed().forEach { bit in
+            ans = (ans * modPow(x, bit)) % mod
+            x = modPow(x, 10) % mod
+        }
+        return ans
+    }
 }
