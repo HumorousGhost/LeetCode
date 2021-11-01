055a8af44c520dd70727127efa489f7adc7e91b9
zhangliqun <zhangliuqn@xiangshang360.com>
2021-11-01 10:06:29 +0800

    375. 猜数字大小 II

--- LeetCode/ThirtySeven.swift
+++ LeetCode/ThirtySeven.swift
@@ -112,4 +112,31 @@ class ThirtySeven: GuessGame {
         }
         return left
     }
+    
+    // 375. 猜数字大小 II
+    func getMoneyAmout(_ n: Int) -> Int {
+        
+        var sign = [[Int]](repeating: [Int](repeating: 0, count: n + 1), count: n + 1)
+        
+        func calculate(_ left: Int, _ right: Int) -> Int {
+            guard left < right else {
+                return 0
+            }
+            
+            if sign[left][right] != 0 {
+                return sign[left][right]
+            }
+            
+            var minres = Int.max
+            let mid = (left + right) / 2
+            for i in mid...right {
+                let res = i + max(calculate(i + 1, right), calculate(left, i - 1))
+                minres = min(res, minres)
+            }
+            sign[left][right] = minres
+            return minres
+        }
+        
+        return calculate(1, n)
+    }
 }
