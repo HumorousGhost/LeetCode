9f92e916b090a3c490c4adfc7e6ca5e404f1dbdc
zhangliqun <zhangliuqn@xiangshang360.com>
2021-11-02 11:37:00 +0800

    377. 组合总和 IV

--- LeetCode/ThirtySeven.swift
+++ LeetCode/ThirtySeven.swift
@@ -166,4 +166,19 @@ class ThirtySeven: GuessGame {
         
         return max(up[n - 1], down[n - 1])
     }
+    
+    // 377. 组合总和 IV
+    func combinationSum4(_ nums: [Int], _ target: Int) -> Int {
+        var dp = [Int](repeating: 0, count: target + 1)
+        dp[0] = 1
+        for i in 1...target {
+            for num in nums {
+                if num <= i {
+                    dp[i] &+= dp[i - num] // 溢出运算符
+                }
+            }
+        }
+        
+        return dp[target]
+    }
 }
