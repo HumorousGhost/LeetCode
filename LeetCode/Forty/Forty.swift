cec0f96772d9c9305b1cad200dfcb68e69571182
zhangliqun <zhangliuqn@xiangshang360.com>
2021-12-02 11:19:42 +0800

    change

---
+++ LeetCode/Forty/Forty.swift
@@ -0,0 +1,16 @@
+//
+//  Forty.swift
+//  LeetCode
+//
+//  Created by UED on 2021/12/2.
+//
+
+import Foundation
+
+class Forty {
+    // 400. 第 N 位数字
+    func findNthDigit(_ n: Int) -> Int {
+        
+        return 0
+    }
+}
