ec7b66ef70aca8148a7be221b3ec153107060953
zhangliqun <zhangliuqn@xiangshang360.com>
2021-12-06 10:22:14 +0800

    400. 第 N 位数字

--- LeetCode/Forty/Forty.swift
+++ LeetCode/Forty/Forty.swift
@@ -11,6 +11,32 @@ class Forty {
     // 400. 第 N 位数字
     func findNthDigit(_ n: Int) -> Int {
         
-        return 0
+        func totalDigits(_ length: Int) -> Int {
+            var digits = 0, curLength = 1, curCount = 9
+            while curLength <= length {
+                digits += curLength * curCount
+                curLength += 1
+                curCount *= 10
+            }
+            return digits
+        }
+        
+        var low = 1, high = 9
+        while low < high {
+            let mid = (high - low) / 2 + low
+            if totalDigits(mid) < n {
+                low = mid + 1
+            } else {
+                high = mid
+            }
+        }
+        let d = low
+        let prevDigits = totalDigits(d - 1)
+        let index = n - prevDigits - 1
+        let start = Int(pow(10.0, Double(d - 1)))
+        let num = start + index / d
+        let digitIndex = index % d
+        let digit = (num / Int(pow(10.0, Double(d - digitIndex - 1)))) % 10
+        return digit
     }
 }
