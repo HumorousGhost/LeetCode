f71896908255e77a95be3c2cdef51620b62add9b
zhangliqun <zhangliuqn@xiangshang360.com>
2022-01-13 11:00:59 +0800

    423. 从英文中重建数字

--- LeetCode/Forty/FortyTwo.swift
+++ LeetCode/Forty/FortyTwo.swift
@@ -209,4 +209,32 @@ struct FortyTwo {
         }
         return x
     }
+    
+    // 423. 从英文中重建数字
+    func originalDigits(_ s: String) -> String {
+        var c = [Character: Int]()
+        for char in s {
+            c[char, default: 0] += 1
+        }
+        
+        var cnt = [Int](repeating: 0, count: 10)
+        cnt[0] = c["z"] ?? 0
+        cnt[2] = c["w"] ?? 0
+        cnt[4] = c["u"] ?? 0
+        cnt[6] = c["x"] ?? 0
+        cnt[8] = c["g"] ?? 0
+        cnt[3] = (c["h"] ?? 0) - cnt[8]
+        cnt[5] = (c["f"] ?? 0) - cnt[4]
+        cnt[7] = (c["s"] ?? 0) - cnt[6]
+        cnt[1] = (c["o"] ?? 0) - cnt[0] - cnt[2] - cnt[4]
+        cnt[9] = (c["i"] ?? 0) - cnt[5] - cnt[6] - cnt[8]
+        
+        var ans = ""
+        for i in 0..<10 {
+            for _ in 0..<cnt[i] {
+                ans.append("\(i)")
+            }
+        }
+        return ans
+    }
 }
